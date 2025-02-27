24b5c9444b1a898c241ca5da1d6fd4eb2050c1c3
herizador <155181668+herizador@users.noreply.github.com>
2025-02-27 01:09:10 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const slider = document.getElementById("opacity-slider");
-    const container = document.querySelector(".container");
 
     slider.addEventListener("input", () => {
-        slider.addEventListener("input", () => {
-    container.style.backgroundColor = `rgba(255, 255, 255, ${slider.value})`;
-});
+        document.documentElement.style.setProperty("--background-opacity", slider.value);
     });
 });
+
