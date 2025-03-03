58638d10a591027166303357828213ead6fd556b
herizador <155181668+herizador@users.noreply.github.com>
2025-03-04 00:13:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -283,3 +283,11 @@ io.on("connection", (socket) => {
 app.listen(PORT, () => {
   console.log(`🚀 Servidor en http://localhost:${PORT}`);
 });
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "script-src 'self' https://cdn.jsdelivr.net;"
+  );
+  next();
+});
